eb8006ab2203ad3c5ef01b8639f3fb4764a4c71e
michaeldurfey <mdurfey@me.com>
2018-01-15 11:37:59 -0800

    basic working stringifyJSONSpec

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj !== 'object' || obj === null) {
+    if (typeof obj === 'string') {
+      return String(`"${obj}"`);
+    }
+    return String(`${obj}`);
+  } else {
+    // Arrays
+    if (Array.isArray(obj)) {
+      return '[' + obj.map(function(item) {
+        return stringifyJSON(item);
+      }).join(',') + ']';
+    }
+    // Objects
+    var objArr = [];
+    
+    for (let x in obj) {
+      if (typeof obj[x] === 'function' || typeof obj[x] === 'undefined') {
+        continue;
+      }
+      objArr.push(`"${x}":${stringifyJSON(obj[x])}`);
+    }
+    return '{' + objArr.join(',') + '}';
+  }
 };
